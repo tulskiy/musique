019f7fc8783ff981381eb71c22edd7acc3277826
tulskiy <tulskiy@tulskiy-laptop.(none)>
2010-06-08 15:06:36 +0600

    add: use JAVA_PATH variable to allow using alternative JRE's on 64-bit JRE's

--- musique.sh
+++ musique.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 MUSIQUE_HOME=`dirname "$0"`
 cd "$MUSIQUE_HOME"
-exec java -client -Xms40m -Xmx60m -Djava.library.path=native -jar musique.jar
+
+# change this to use alternative JRE. If you are using 64-bit JRE,
+# it is recommended to install ia32-sun-java6-jre to reduce memory
+# and CPU usage, and set JAVA_PATH to eg. /usr/lib/jvm/ia32-java-6-sun/bin/java
+export JAVA_PATH="java"
+
+exec $JAVA_PATH -client -Xms30m -Xmx60m -Djava.library.path=native:/usr/lib -jar musique.jar
