1ebc77d74e6927a8d3466d0307e30cdeabf659aa
tulskiy <tulskiy@tulskiy-laptop.(none)>
2010-06-09 20:42:05 +0600

    added .gitignore

---
+++ musique-32bit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+MUSIQUE_HOME=`dirname "$0"`
+cd "$MUSIQUE_HOME"
+
+# change this to use alternative JRE. If you are using 64-bit JRE,
+# it is recommended to install ia32-sun-java6-jre to reduce memory
+# and CPU usage, and set JAVA_PATH to eg. /usr/lib/jvm/ia32-java-6-sun/bin/java
+export JAVA_PATH="/usr/lib/jvm/ia32-java-6-sun/bin/java"
+
+exec $JAVA_PATH -client -Xms30m -Xmx60m -Djava.library.path=native:/usr/lib -jar musique.jar
