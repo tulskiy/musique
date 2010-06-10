f5d6f834b744ecb61aaf14f399092e03cbcf55cd
tulskiy <tulskiy@gmail.com>
2010-06-10 17:05:37 +0600

    added another solution for sound problems in sun java

--- musique-padsp.sh
+++ musique-padsp.sh
@@ -5,6 +5,6 @@ cd "$MUSIQUE_HOME"
 # change this to use alternative JRE. If you are using 64-bit JRE,
 # it is recommended to install ia32-sun-java6-jre to reduce memory
 # and CPU usage, and set JAVA_PATH to eg. /usr/lib/jvm/ia32-java-6-sun/bin/java
-export JAVA_PATH="/usr/lib/jvm/ia32-java-6-sun/bin/java"
+export JAVA_PATH="java"
 
 exec padsp $JAVA_PATH -client -Xms30m -Xmx60m -Djava.library.path=native:/usr/lib -jar musique.jar
