01f0f989477208e9e8d27d80d33c927c6bd85e68
tulskiy <tulskiy@gmail.com>
2010-06-18 02:26:25 +0600

    fixed negative seek offset exception

--- modules/jmac/src/davaguine/jmac/info/APETagFooter.java
+++ modules/jmac/src/davaguine/jmac/info/APETagFooter.java
@@ -1,4 +1,21 @@
 /*
+ * Copyright (c) 2008, 2009, 2010 Denis Tulskiy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  *  21.04.2004 Original verion. davagin@udm.ru.
  *-----------------------------------------------------------------------
  *  This program is free software; you can redistribute it and/or modify
@@ -33,11 +50,11 @@ import java.io.IOException;
  * Time: 14:51:31
  */
 public class APETagFooter {
-    public String m_cID;		    	// should equal 'APETAGEX' (char[8])
-    public int m_nVersion;			// equals CURRENT_APE_TAG_VERSION (int)
-    public int m_nSize;				// the complete size of the tag, including this footer (int)
-    public int m_nFields;	    		// the number of fields in the tag (int)
-    public int m_nFlags;				// the tag flags (none currently defined) (int)
+    public String m_cID;                // should equal 'APETAGEX' (char[8])
+    public int m_nVersion;            // equals CURRENT_APE_TAG_VERSION (int)
+    public int m_nSize;                // the complete size of the tag, including this footer (int)
+    public int m_nFields;                // the number of fields in the tag (int)
+    public int m_nFlags;                // the tag flags (none currently defined) (int)
 
     public final static int APE_TAG_FOOTER_BYTES = 32;
 
@@ -89,9 +106,9 @@ public class APETagFooter {
 
     public boolean GetIsValid(boolean bAllowHeader) {
         boolean bValid = m_cID.equals("APETAGEX") &&
-                (m_nVersion <= CURRENT_APE_TAG_VERSION) &&
-                (m_nFields <= 65536) &&
-                (GetFieldBytes() <= (1024 * 1024 * 16));
+                         (m_nVersion <= CURRENT_APE_TAG_VERSION) &&
+                         (m_nFields <= 65536) &&
+                         (GetFieldBytes() <= (1024 * 1024 * 16));
 
         if (bValid && !bAllowHeader && GetIsHeader())
             bValid = false;
@@ -103,6 +120,8 @@ public class APETagFooter {
     public static APETagFooter read(final File file, boolean skipId3) throws IOException {
         long pos = file.length() - APE_TAG_FOOTER_BYTES;
         if (skipId3) pos -= ID3Tag.ID3_TAG_BYTES;
+        if (pos < 0)
+            return null;
         file.seek(pos);
         APETagFooter tag = new APETagFooter();
         try {
