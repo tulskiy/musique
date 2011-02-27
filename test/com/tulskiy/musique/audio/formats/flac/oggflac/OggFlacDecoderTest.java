38060adf054c2dac9ecbabc622e7c80da660159d
Denis Tulskiy <tulskiy@gmail.com>
2011-02-27 20:30:40 +0600

    tests: disable old tests

--- test/com/tulskiy/musique/audio/formats/flac/oggflac/OggFlacDecoderTest.java
+++ test/com/tulskiy/musique/audio/formats/flac/oggflac/OggFlacDecoderTest.java
@@ -1,30 +1,30 @@
 /*
- * Copyright (c) 2008, 2009 Denis Tulskiy
+ * Copyright (c) 2008, 2009, 2010, 2011 Denis Tulskiy
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.tulskiy.musique.audio.formats.flac.oggflac;
 
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.kc7bfi.jflac.metadata.VorbisComment;
 import org.kc7bfi.jflac.metadata.Padding;
+import org.kc7bfi.jflac.metadata.VorbisComment;
 
-import java.io.RandomAccessFile;
 import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @Author: Denis Tulskiy
@@ -33,7 +33,7 @@ import java.io.FileNotFoundException;
 public class OggFlacDecoderTest {
     OggFlacDecoder oggFlacDecoder;
 
-    @Before
+    //    @Before
     public void setUp() {
         oggFlacDecoder = new OggFlacDecoder();
         try {
@@ -43,7 +43,7 @@ public class OggFlacDecoderTest {
         }
     }
 
-//    @Test
+    //    @Test
     public void testMetadata() {
         assertEquals(1, oggFlacDecoder.majorVersion);
         assertEquals(0, oggFlacDecoder.minorVersion);
--- test/com/tulskiy/musique/audio/player/dsp/DSPSpeedTest.java
+++ test/com/tulskiy/musique/audio/player/dsp/DSPSpeedTest.java
@@ -1,34 +1,30 @@
 /*
- * Copyright (c) 2008, 2009 Denis Tulskiy
+ * Copyright (c) 2008, 2009, 2010, 2011 Denis Tulskiy
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.tulskiy.musique.audio.player.dsp;
 
-import org.junit.Test;
-
 import java.util.ArrayList;
-import java.util.Vector;
-import java.util.LinkedList;
 
 /**
  * @Author: Denis Tulskiy
  * @Date: 26.07.2009
  */
 public class DSPSpeedTest {
-    @Test
+    //    @Test
     public void test() {
         ArrayList<String> l = new ArrayList<String>();
         String[] a = new String[1000000];
@@ -45,8 +41,8 @@ public class DSPSpeedTest {
         System.out.println(System.currentTimeMillis() - time);
 
         time = System.currentTimeMillis();
-        for (String i: a) {
-            
+        for (String i : a) {
+
         }
         System.out.println(System.currentTimeMillis() - time);
     }
