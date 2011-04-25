95485d8996268cf207649030bea99312e2272687
Maksim Liauchuk <maksim_liauchuk@lavabit.com>
2011-04-25 14:15:19 +0300

    First commit

--- modules/aac/src/net/sourceforge/jaad/impl/error/package-info.java
+++ modules/aac/src/net/sourceforge/jaad/impl/error/package-info.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package jaad.impl.error;
+package net.sourceforge.jaad.impl.error;
 
 /**
 * Error resilience and error protection classes.
