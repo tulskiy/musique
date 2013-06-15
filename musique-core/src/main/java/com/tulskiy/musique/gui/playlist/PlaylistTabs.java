a14626c7a3d854fedfcc6bb2dfc4071552394235
Denis Tulskiy <tulskiy@gmail.com>
2013-06-15 18:35:15 +0600

    use commons and fix some exceptions in formatting playlist title

--- musique-core/src/main/java/com/tulskiy/musique/gui/playlist/PlaylistTabs.java
+++ musique-core/src/main/java/com/tulskiy/musique/gui/playlist/PlaylistTabs.java
@@ -8,11 +8,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>
+ * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.tulskiy.musique.gui.playlist;
